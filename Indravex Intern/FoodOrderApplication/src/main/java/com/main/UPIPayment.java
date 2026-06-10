dd6f3253efe5e4065f512e214b70ad0edaf7d866
Bhavesh Shinde <bhaveshshinde.dev@gmail.com>
2026-06-10 15:09:03 +0530

    Delete Indravex Intern directory

--- Indravex Intern/FoodOrderApplication/src/main/java/com/main/UPIPayment.java
+++
@@ -1,8 +0,0 @@
-package com.main;
-
-class UPIPayment implements Payment {
-    @Override
-    public void pay(double amount) {
-        System.out.println("Paid ₹" + amount + " using UPI.");
-    }
-}
